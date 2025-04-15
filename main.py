d13ee9f126b49bc8441b6f332a787ed04d6f48eb
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2025-04-15 19:53:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dataset import create_wall_dataloader
 from evaluator import ProbingEvaluator
 import torch
-from models import MockModel
+from models import MockModel, JEPAAgent
 import glob
 
 
