40d69bc4f6a86822402aabd4c97e475b63644304
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2025-04-18 05:20:31 -0400

    Update models.py

--- models.py
+++ models.py
@@ -196,6 +196,7 @@ class JEPAAgent(nn.Module):
             a_t = actions[:, t]  # [B, 2]
             a_embed = self.action_encoder(a_t)  # [B, action_emb_dim]
             inp = torch.cat([s_prev, a_embed], dim=-1)  # [B, repr_dim + action_emb_dim]
+            inp = inp.unsqueeze(1)  #for GRU
             #inp = torch.cat([s_prev, a_t], dim=-1)  # [B, repr_dim+2]
             #s_pred = self.predictor(inp)  # [B, repr_dim]
             s_pred, h = self.predictor(inp, h) 
