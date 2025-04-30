f3648fdc9dc79e9f2ac26c912bedce848ebefcc4
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2025-04-30 13:25:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from evaluator import ProbingEvaluator
 from models import JEPAAgent
 import torch.nn.functional as F
 from normalizer import Normalizer
+
+import random
+import numpy as np
+import torch
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # for multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+# Call at very beginning of main.py
+set_seed(42)
 normalizer = Normalizer()
 
 
